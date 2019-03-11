7731679b4e2ae26455fe4c714a3431058a703743
Atsushi Kawamura <akawamura@c-fo.com>
2019-03-11 14:06:49 +0900

    add s3 sync command

--- src/handlers/sync/post.js
+++ src/handlers/sync/post.js
@@ -1,13 +1,23 @@
 import {spawnSync} from 'child_process';
+import config from '../../common/config';
+import { join as joinPath, normalize as normalizePath, extname as pathExtension } from 'path';
 
 export default function syncPostHandler(request, response) {
   /**
    * @type {ElastalertServer}
    */
   var s3_bucket = process.env.S3_BUCKET_NAME;
+  var path = ""
+
+  const ruleFolderSettings = config.get('rulesPath');
+  if (ruleFolderSettings.relative) {
+    path = joinPath(config.get('elastalertPath'), ruleFolderSettings.path);
+  } else {
+    path = ruleFolderSettings.path;
+  }
 
   if (s3_bucket) {
-    var syncResult = spawnSync('aws', ['s3', 'ls', '']);
+    var syncResult = spawnSync('aws', ['s3', 'sync', 's3://'+s3_bucket, path, '--delete']);
     response.send(syncResult.stdout.toString());
   } else {
     response.send({
